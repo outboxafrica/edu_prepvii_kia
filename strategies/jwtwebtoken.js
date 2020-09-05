10d32a4b1f0007560591f25da1bf1a56e496d36c
SamuelMabonga <samuelmabonga256@gmail.com>
2020-09-05 13:36:11 +0300

    JWT Strategy

---
+++ strategies/jwtwebtoken.js
@@ -0,0 +1,27 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt 
+const mongoose = require('mongoose')
+
+// import schema to use with jwt
+const User = mongoose.model('myUser')
+// import DB secret key
+const secretkey = require('../config/myDbUrl')
+
+
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+opts.secretOrKey = secretkey.secretOrKey
+
+
+module.exports = passport => {
+  passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    User.findById(jwt_payload.id)
+      .then(person => {
+        if (person) {
+          return done(null, person)
+        }
+        return (null, false)
+      })
+      .catch(err => console.log(err))
+  }))
+}
