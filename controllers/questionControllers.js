05ee56b8209aa668ec2f3a1bc20fc41bad281757
SamuelMabonga <samuelmabonga256@gmail.com>
2020-09-05 13:36:54 +0300

    Qestion Controllers

---
+++ controllers/questionControllers.js
@@ -0,0 +1,12 @@
+// Fetch Question Model
+const Question = require('../model/question')
+// Fetch Profile model
+const Profile = require('../model/profile')
+// Fetch User model
+const User = require('../model/user')
+
+// Fetch Modules
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+
