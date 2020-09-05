01e7d16027ff336c7bab5f621b4c9a4d1333c5a9
SamuelMabonga <samuelmabonga256@gmail.com>
2020-09-05 18:58:56 +0300

    Update a question

--- controllers/questionControllers.js
+++ controllers/questionControllers.js
@@ -24,3 +24,41 @@ exports.post = async (req, res) => {
     })
     .catch(err => console.log("Unable to save question to database", err))
 }
+
+// update a question
+exports.update = async (req, res) => {
+  console.log("req.body: ", req.body)
+  // inserting a update
+  let question_id = req.body._id
+  let update = {
+    textone: req.body.textone,
+    texttwo: req.body.texttwo
+  };
+
+  // database work logic
+  Question.findOneAndUpdate(
+    { 
+      _id: question_id,
+      user: req.user.id
+    },
+    { $set: update },
+    { new: true }, 
+  ).then(question => {
+    res.status(200).json(question)
+  }).catch(err => console.log(`**ERROR** : ${err}`))
+
+  // Question.findByIdAndUpdate(
+  //   _id, 
+  //   update, 
+  //   { new: true }, 
+  //   (err, update) => {
+  //     if (err) {
+  //       console.log("err: ", err)
+  //       res.status(500).send(err)
+  //     } else {
+  //       console.log("success")
+  //       res.send(update)
+  //     }
+  //   }
+  // )
+}
--- routes/questions.js
+++ routes/questions.js
@@ -6,9 +6,15 @@ const passport = require('passport')
 const controllers = require('../controllers/questionControllers')
 
 // @type - POST
-// @route - /api/auth/login
+// @route - /questions/post
 // @desc - route for logging in users
-// @access - PUBLIC
+// @access - PRIVATE
 router.post("/post", passport.authenticate('jwt', { session: false}), controllers.post)
+
+// @type - POST
+// @route - /questions/update
+// @desc - route for updating a question
+// @access - PRIVATE
+router.post("/update", passport.authenticate('jwt', { session: false }), controllers.update)
 
 module.exports = router
