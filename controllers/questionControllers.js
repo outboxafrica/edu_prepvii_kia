2560f0c63533306a5dd520506a3e7ae8bb514fb2
SamuelMabonga <samuelmabonga256@gmail.com>
2020-09-07 22:11:49 +0300

    User can post answer - finishes #174696421

--- controllers/questionControllers.js
+++ controllers/questionControllers.js
@@ -22,3 +22,35 @@ exports.post = async (req, res) => {
     })
     .catch(err => console.log("Unable to save question to database", err))
 }
+
+// Post an answer to a question
+exports.postAnswer = async (req, res) => {
+  let question_id = req.params.question_id
+  console.log(question_id)
+
+  let newAnswer = new Answer(
+    {
+      user: req.user.id,
+      text: req.body.text,
+      name: req.user.name,
+    }
+  )
+
+  newAnswer.save((err) => {
+    if (err) return console.log(`**ERROR** saving answer: ${err}`)
+
+    Question.findOneAndUpdate(
+      { 
+        _id: question_id
+      },
+      { $push: { "answers.0": newAnswer }},
+      { new: true}
+    ).then(question => {
+      console.log(question)
+      res.status(200).json(question)
+    }).catch(err => {
+      console.log(`**ERROR** find and update question : ${err}`)
+      res.status(500).json({ failedToUpdate: "Failed to save the answer!"})
+    })
+  })
+}
