7de0519def135502b76b5a143591e0b7ed709193
SamuelMabonga <samuelmabonga256@gmail.com>
2020-09-09 04:23:53 +0300

    Fixed bugs

--- routes/questions.js
+++ routes/questions.js
@@ -27,18 +27,18 @@ router.delete("/", passport.authenticate("jwt", { session: false }), controllers
 // @route - /questions/answers/:question_id
 // @desc - route for viewing answers to a question
 // @access - PUBLIC
-router.get("/answers/:question_id/get", controllers.getAnswers)
+router.get("/:question_id/answers", controllers.getAnswers)
 
 
 // @type - POST
-// @route - /questions/:question_id/answers/:answer_id
+// @route - /questions/:question_id/accept
 // @desc - route for accepting a preferred answer
 // @access - PRIVATE
 router.post("/:question_id/accept", passport.authenticate("jwt", { session: false }), controllers.acceptAnswer)
 
 // @type - GET
 // @route - /questions
-// @desc - get all questions written by the user
+// @desc - get all questions
 // @access - PUBLIC
 router.get("/", controllers.getQuestions)
 
