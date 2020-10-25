1ff5b91351b9238aa0e765003b8ecf149b21fc76
SamuelMabonga <samuelmabonga256@gmail.com>
2020-10-25 17:48:45 +0300

    Changes for the client.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 // import passport
 const passport = require('passport');
+// import cors
+const cors = require('cors');
 
 // Create a port variable
 const port = process.env.PORT || 4000;
@@ -25,6 +27,20 @@ const questionsRoute = require('./routes/questions');
 // create an express app
 const app = express();
 
+// const corsOptions = {
+//   origin: 'http://localhost:3000',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+
+// use cors
+app.use(cors())
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "*");
+//   next();
+// });
+
 // use express.json middleware
 app.use(express.json());
 // express.static to enable us access static files
@@ -42,6 +58,12 @@ mongoose.connect(DB, { useUnifiedTopology: true, useNewUrlParser: true, useFindA
 // Passport Middleware
 app.use(passport.initialize());
 
+
+
+
+
+
+
 // config for jwt token
 require('./strategies/jwtwebtoken')(passport);
 
@@ -49,7 +71,7 @@ require('./strategies/jwtwebtoken')(passport);
 // use auth routes
 app.use('/api', signuproute);
 // use questions routes
-app.use('/api/questions', questionsRoute);
+app.use('/api/questions', cors(), questionsRoute);
 
 // check port
 app.listen(port, () => {
